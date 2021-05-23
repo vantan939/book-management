8052ecf4854a5539978390732ad2067ed48fed5e
Tan Nguyen <vantan939@gmail.com>
2021-05-23 22:09:45 +0700

    Create views basic

---
+++ resources/views/pages/book-list.blade.php
@@ -0,0 +1,14 @@
+@extends('app')
+
+@section('content')
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12 title-page">
+				<h2>BOOK LIST</h2>
+			</div>
+		</div>
+		<div class="row">
+			
+		</div>
+	</div>
+@endsection
---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,5 @@
+<footer>
+	<div class="container">
+		<p>Copyright @2021 - Books Management</p>
+	</div>		
+</footer>
